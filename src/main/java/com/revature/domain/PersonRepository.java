528ecd0bfbda285dc3b3e7e455b68a58619994f7
Yannick <yhueftle@gmail.com>
2017-03-02 16:53:38 -0700

    create placeholder getAllMembersByBranch methods

--- src/main/java/com/revature/domain/PersonRepository.java
+++ src/main/java/com/revature/domain/PersonRepository.java
@@ -2,12 +2,20 @@ package com.revature.domain;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonRepository extends CrudRepository<Person,Integer> {
+public interface PersonRepository extends JpaRepository<Person,Integer> {
 
-	List<Person> findByLastName(String lastName);
+	List<Person> findAllByLastName(String lastName);
+	List<Person> findAllByFirstName(String firstName);
+	List<Person> findAllByFirstNameAndLastName(String firstName, String lastName);
 	
+	@Query("SELECT ie_person.p_firstname, ie_person.p_lastname, ie_person.p_id FROM ie_person INNER JOIN ie_eval ON ie_person.p_id=ie_eval.e_pid_trainee WHERE ie_eval.e_batch=?1")
+	List<Person> findAllByBatchId(int id);
+	
+	@Query("SELECT ie_person.p_firstname, ie_person.p_lastname, ie_person.p_id FROM ie_person INNER JOIN ie_eval ON ie_person.p_id=ie_eval.e_pid_trainee INNER JOIN ie_person ON ie_eval.e_batch=ie_batch.b_id WHERE b_name=?1")
+	List<Person> findAllByBatchName(String batchName);
 }
