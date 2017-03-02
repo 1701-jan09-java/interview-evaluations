18bf6af027eeb3afe999a812de04b3969cb8acea
sharangw <sharang@sharangs-mbp.mobile.asu.edu>
2017-03-02 10:39:57 -0700

    add Spring data example for Person

---
+++ src/main/java/com/revature/domain/PersonRepository.java
@@ -0,0 +1,11 @@
+package com.revature.domain;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonRepository extends CrudRepository<Person,Integer> {
+
+	List<Person> findByName(String name);
+	
+}
