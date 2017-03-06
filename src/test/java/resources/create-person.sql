0e57f9a9129461239ec7e4c190072756b92e03c6
sharangw <sharang.wakankar@hotmail.com>
2017-03-05 17:44:14 -0700

    add minor changes to test

--- src/test/java/resources/create-person.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE ie_person (
-    p_id INTEGER PRIMARY KEY,
-    p_firstname VARCHAR(4000),
-    p_lastname VARCHAR(4000)
-);
-
-CREATE SEQUENCE person_seq
-START WITH 100
-INCREMENT BY 1;
