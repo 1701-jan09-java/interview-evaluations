63421b8354819ff23144aa51250921c398151249
Tembong Fonji <fonji@email.arizona.edu>
2017-03-06 14:54:46 -0700

    edit PersonRole.java

--- src/main/java/com/revature/domain/PersonRole.java
+++ src/main/java/com/revature/domain/PersonRole.java
@@ -16,4 +16,64 @@ public class PersonRole {
 	@Column(name = "pr_title")
 	private String title;
 
+	public PersonRole(int id, String title) {
+		super();
+		this.id = id;
+		this.title = title;
+	}
+
+	public PersonRole(){
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PersonRole other = (PersonRole) obj;
+		if (id != other.id)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonRole [id=" + id + ", title=" + title + "]";
+	}
+	
+	
 }
