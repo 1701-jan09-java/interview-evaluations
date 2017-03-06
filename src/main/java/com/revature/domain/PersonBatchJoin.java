3f4ae642d116d7607232c03fb7d209d644ee152f
Tembong Fonji <fonji@email.arizona.edu>
2017-03-06 13:57:57 -0700

    add @RequestMapping for /batch/{id}/members

--- src/main/java/com/revature/domain/PersonBatchJoin.java
+++ src/main/java/com/revature/domain/PersonBatchJoin.java
@@ -28,4 +28,77 @@ public class PersonBatchJoin {
 	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "batch_id", nullable = false)
 	private Batch batch;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
+	public Batch getBatch() {
+		return batch;
+	}
+
+	public void setBatch(Batch batch) {
+		this.batch = batch;
+	}
+
+	public PersonBatchJoin(){}
+	
+	public PersonBatchJoin(int id, Person person, Batch batch) {
+		super();
+		this.id = id;
+		this.person = person;
+		this.batch = batch;
+	}
+
+	@Override
+	public String toString() {
+		return "PersonBatchJoin [id=" + id + ", person=" + person + ", batch=" + batch + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((batch == null) ? 0 : batch.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((person == null) ? 0 : person.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PersonBatchJoin other = (PersonBatchJoin) obj;
+		if (batch == null) {
+			if (other.batch != null)
+				return false;
+		} else if (!batch.equals(other.batch))
+			return false;
+		if (id != other.id)
+			return false;
+		if (person == null) {
+			if (other.person != null)
+				return false;
+		} else if (!person.equals(other.person))
+			return false;
+		return true;
+	}
+	
 }
