f69a15751ec0e97d3886cd5a7e054e659f5af49e
sharangw <sharang.wakankar@hotmail.com>
2017-03-10 10:39:53 -0700

    remove PersonEntityTest

--- src/main/java/com/revature/repositories/PersonRepository.java
+++ src/main/java/com/revature/repositories/PersonRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.revature.domain.Batch;
 import com.revature.domain.Person;
 import com.revature.domain.PersonRole;
 
