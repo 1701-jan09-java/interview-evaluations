254fa40c0b3f34c23927b2bfca83f2051419cb63
Madison <madison.f.redd@live.com>
2017-03-15 11:42:07 -0700

    minor bugfix, code clean, beautification

--- src/main/java/com/revature/controllers/EvalController.java
+++ src/main/java/com/revature/controllers/EvalController.java
@@ -11,6 +11,7 @@ import org.springframework.data.web.SortDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/api/v1/evaluations")
 public class EvalController {
--- src/main/java/com/revature/controllers/QuestionController.java
+++ src/main/java/com/revature/controllers/QuestionController.java
@@ -10,6 +10,7 @@ import org.springframework.data.web.SortDefault.SortDefaults;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value="/api/v1/questions")
 public class QuestionController {
